b0f5628bab7f4f717b19541fc112e158262ac79e
Max Fuchs <max.fuchs13@gmail.com>
2024-01-06 11:49:12 +0100

    Add sound functionality

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-// Function to request permission
+// Function to request accelerometer permission
 function requestAccelerometerPermission() {
-    // Check if DeviceMotionEvent is available
     if (typeof DeviceMotionEvent !== 'undefined' && typeof DeviceMotionEvent.requestPermission === 'function') {
         DeviceMotionEvent.requestPermission()
             .then(permissionState => {
@@ -12,30 +11,34 @@ function requestAccelerometerPermission() {
             })
             .catch(console.error);
     } else {
-        // Handle regular non-iOS 13+ devices
         setupAccelerometerEvents();
     }
 }
 
-// Function to setup accelerometer events
 function setupAccelerometerEvents() {
     window.addEventListener('devicemotion', event => {
         let x = event.acceleration.x;
         let y = event.acceleration.y;
         let z = event.acceleration.z;
-        let intensity = Math.sqrt(x*x + y*y + z*z); // Calculate the intensity
+        let intensity = Math.sqrt(x*x + y*y + z*z);
 
         document.querySelectorAll('.drum-pad').forEach(pad => {
-            pad.addEventListener('click', () => playDrumSound(pad.id, intensity));
+            pad.addEventListener('click', () => {
+                pad.classList.add('pressed');
+                playDrumSound(pad.id, intensity);
+                
+                setTimeout(() => {
+                    pad.classList.remove('pressed');
+                }, 100);
+            });
         });
     });
 }
 
 function playDrumSound(drumType, intensity) {
-    // Map drumType to a specific sound file
     let soundFile = getSoundFile(drumType); 
     let audio = new Audio(soundFile);
-    audio.volume = Math.min(intensity / 10, 1); // Adjust volume based on intensity
+    audio.volume = Math.min(intensity / 10, 1);
     audio.play();
 }
 
@@ -48,6 +51,4 @@ function getSoundFile(drumType) {
     }
 }
 
-// Trigger the permission request
 document.getElementById("request-permission").addEventListener('click', requestAccelerometerPermission);
-
